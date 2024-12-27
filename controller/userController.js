6116c272dd8a590c20d1cacb09486979e991cac6
zakya sharmeen <sakarim9124@gmail.com>
2024-12-28 00:03:42 +0530

    crud-node

--- controller/userController.js
+++ controller/userController.js
@@ -20,13 +20,56 @@ const create = async (req, res) => {
     }
 };
 
-// for checking if this works
+// for updating the data into the database
+const update = async (req, res) => {
+    try {
+        const id = req.params.id
+        const userExist = await User.findOne({_id: id})
+        if (!userExist) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        const updateUser = await User.findByIdAndUpdate(id, req.body, {new: true})
+        res.status(201).json(updateUser)
+       
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+
+// for deleting the data into the database
+const deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id
+        const userExist = await User.findOne({_id: id})
+        if (!userExist) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        await User.findByIdAndDelete(id)
+        res.status(201).json({message: "User deleted successfully" })
+       
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+
+
+
+
+// for checking if this works and fetching the dta
 const fetch = async (req, res) => {
     try {
-        return res.json("hello world miss zakya");
+        // return res.json("hello world miss zakya");
+        const users = await User.find()
+        if(users.length === 0){
+            return res.status(404).json({message: "User Not Found"})
+        }
+        res.status(200).json(users)
     } catch (error) {
         res.status(500).json({ error: "Internal Server Error" });
     }
 };
 
-module.exports = { fetch, create };
+module.exports = { fetch, create, update, deleteUser };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,16 @@
 const express = require("express");
 // const userController = require("../controller/userController.js");
 
-const {fetch, create} = require("../controller/userController");
+const {fetch, create, update, deleteUser} = require("../controller/userController");
 
 const route = express.Router();
 
 
-route.get("/fetch", fetch);
+route.get("/getAllUsers", fetch);
 route.post("/create", create)
+route.put("/update/:id", update);
+route.delete("/delete/:id", deleteUser);
+
+
 
 module.exports = route;
